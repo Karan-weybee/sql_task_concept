7a9cc185f8aef662a9da684bb10ba1174d725ea9
Karan-weybee <140693689+Karan-weybee@users.noreply.github.com>
2023-09-25 11:45:40 +0530

    Add files via upload

---
+++ SQL_ROWNUM_ADV_3.sql
@@ -0,0 +1,42 @@
+CREATE TABLE EMPLOYEES(
+EMPLOYEEID INT PRIMARY KEY,
+NAME VARCHAR(20),
+DEPARTMENT VARCHAR(20),
+SALARY DECIMAL(10,0)
+)
+
+SELECT * FROM EMPLOYEES
+
+/*INSERT INTO EMPLOYEES VALUES
+(4,'SARAH','MARKETING',48000),
+(5,'DAVID','OPERATIONS',42000)*/
+
+SELECT EmployeeID, Name, Department, Salary, 
+ROW_NUMBER() OVER (
+ORDER BY Salary ASC
+) AS RowNumber 
+FROM employees;
+
+SELECT TOP 3 EmployeeID, Name, Department, Salary, 
+ROW_NUMBER() OVER (
+ORDER BY Salary ASC
+) AS RowNumber 
+FROM employees;
+
+SELECT EmployeeID, Name, Department, Salary, 
+RANK() OVER (
+ORDER BY Salary ASC
+) AS RANK_NUM 
+FROM employees;
+
+SELECT EmployeeID, Name, Department, Salary, 
+DENSE_RANK() OVER (
+ORDER BY Salary ASC
+) AS RANK_DENSE 
+FROM employees;
+
+SELECT TOP 3 EmployeeID, Name, Department, Salary, 
+NTILE(3) OVER (
+ORDER BY Salary ASC
+) AS NTILE_NUM 
+FROM employees;
